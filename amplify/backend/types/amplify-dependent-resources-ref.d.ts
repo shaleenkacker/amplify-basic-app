07040487686c0962055206e918437fa870387e3f
shaleenkacker <shaleenkacker@gmail.com>
2024-07-03 13:40:56 -0400

    working example of Amplify App

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "restapi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
+    "amplifybasicapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
   },
-  "function": {
-    "amplifybasicappab8090e6": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+  "auth": {
+    "amplifybasicapp636aa05d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   }
 }
